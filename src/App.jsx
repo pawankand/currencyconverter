0658e18a5500a5c4dd3ed68dfa2f8b4fb53e5d10
pawankand <kand.pawan22@gmail.com>
2023-10-11 19:34:51 +0530

    added custom hooks for currency info

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+  const currencyInfo = useCurrencyInfo(from);
+  const options = Object.keys(currencyInfo);
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to]);
+  };
 
   return (
-     <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat' 
-     style={{backgroundImage: `url(https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1600)` }}>
-       <h1>Currency Converter App</h1>
-     </div> 
-  )
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage: `url(https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1600)`,
+      }}
+    >
+      <h1>Currency Converter App</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,18 +1,53 @@
-import React from 'react'
+import React, { useId } from "react";
 
 function InputBox() {
-    label,
+  label,
     amount,
     onAmountChange,
     onCurrencyChnage,
-    currencyOptions = [],
-    selectedCurrency = "USD",
-    amountDisabled = false,
-    currencyDisabled = false,
-    className = ""
+    (currencyOptions = []),
+    (selectedCurrency = "USD"),
+    (amountDisabled = false),
+    (currencyDisabled = false),
+    (className = "");
+  const id = useId();
   return (
-    <div>InputBox</div>
-  )
+    <div className={`bg-white p-3 rounded-lg text-sm flex`}>
+      <div className="w-1-2">
+        <label htmlFor={id} className="text-black/40 mb-2 inline-block">
+          {label}
+        </label>
+        <input
+          id={id}
+          type="number"
+          className="outline-none w-full bg-transparent py-1.5"
+          placeholder="Amount"
+          disabled={amountDisabled}
+          value={amount}
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
+        ></input>
+      </div>
+      <div className="w-1/2 flex flex-wrap justify-end text-right">
+        <p className="text-black/40 mb-2 w-full">Currency Type</p>
+        <select
+          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+          value={selectedCurrency}
+          onChange={(e) => {
+            onCurrencyChnage && onCurrencyChnage(e.target.value);
+          }}
+          disabled={currencyDisabled}
+        >
+          {currencyOptions.map((currency) => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
 }
 
-export default InputBox
+export default InputBox;
